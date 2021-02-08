ee8d92d1f9039a2e0d6692dc9a4bce7ff3167582
VictorLetirand <vl.victor@hotmail.fr>
2021-02-08 11:05:07 +0100

    Base

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:ardoise_vocale/login.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Formation app',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/textImputWidget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class TextInputWidget extends StatefulWidget {
+  final Function(String) callback;
+
+  TextInputWidget(this.callback);
+
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void click() {
+    widget.callback(controller.text);
+    controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+        controller: this.controller,
+        decoration: InputDecoration(
+            prefixIcon: Icon(Icons.message),
+            labelText: "Type un message:",
+            suffixIcon: IconButton(
+              icon: Icon(Icons.send),
+              tooltip: "Envoyer le message",
+              splashColor: Colors.blue[800],
+              onPressed: () => this.click(),
+            )));
+  }
+}
