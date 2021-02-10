220a89150933c3c879a7d0be5d07a4724c13511f
VictorLetirand <vl.victor@hotmail.fr>
2021-02-10 17:23:05 +0100

    Add Drawer

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'post.dart';
 import 'textImputWidget.dart';
 import 'postList.dart';
+import 'customListTile.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -21,6 +22,37 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Text('hello Men')),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                height: 50.0,
+                child: DrawerHeader(child: Text("This is drawer header")),
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: <Color>[
+                  Colors.blue[900],
+                  Colors.blue[300],
+                ])),
+                margin: EdgeInsets.all(0.0),
+                padding: EdgeInsets.all(0.0),
+              ),
+              CustomListTile(Icons.file_copy_sharp, "Mode d'emploi", () => {}),
+              CustomListTile(
+                  Icons.picture_in_picture_sharp, "Fond d'écran", () => {}),
+              CustomListTile(Icons.color_lens_rounded,
+                  'Changer couleur du texte vocal', () => {}),
+              CustomListTile(Icons.color_lens_rounded,
+                  'Changer couleur du texte écrit', () => {}),
+              CustomListTile(
+                  Icons.phonelink_erase, 'Effacer en touchant', () => {}),
+              CustomListTile(
+                  Icons.priority_high_sharp, 'Ponctuation', () => {}),
+              CustomListTile(Icons.mail, 'Envoi par Mail', () => {}),
+              CustomListTile(
+                  Icons.settings_outlined, 'Paramètres par défaut', () => {}),
+            ],
+          ),
+        ),
         body: Column(children: <Widget>[
           Expanded(child: PostList(this.posts)),
           TextInputWidget(this.newPost)
