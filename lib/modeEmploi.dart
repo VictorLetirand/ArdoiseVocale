019c3dbbefeaf2b183443329d37e2aa6759800a0
VictorLetirand <vl.victor@hotmail.fr>
2021-03-22 13:55:09 +0100

    caroussel + Bouton Text to speech/ trash button

---
+++ lib/modeEmploi.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'myHomePage.dart';
+import 'post.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class ModeEmploi extends StatefulWidget {
+  @override
+  _ModeEmploi createState() => _ModeEmploi();
+}
+
+class _ModeEmploi extends State<ModeEmploi> {
+  List<Post> posts = [];
+  String text = 'Cliquez sur le bouton et commencez à parler';
+  bool isListening = false;
+
+  void newPost(String text) {
+    this.setState(() {
+      posts.add(new Post(text, "Victor"));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mode d'emploi"),
+        leading: IconButton(
+            icon: Icon(Icons.west),
+            onPressed: () => {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()))
+                }),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Center(
+          child: ListView(
+            children: [
+              CarouselSlider(
+                items: [
+                  //1st Image of Slider
+                  Container(
+                    margin: EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: AssetImage("images/flutter-logo.png"),
+                        //fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+
+                  //2nd Image of Slider
+                  Container(
+                    margin: EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: AssetImage("images/logo-final.png"),
+                        //fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+
+                  //3rd Image of Slider
+                  Container(
+                    margin: EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: AssetImage("images/flutter-logo.png"),
+                        //fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+
+                  //4th Image of Slider
+                  Container(
+                    margin: EdgeInsets.all(6.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8.0),
+                      image: DecorationImage(
+                        image: AssetImage("images/logo-v2.png"),
+                        //fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
+                ],
+
+                //Slider Container properties
+                options: CarouselOptions(
+                  height: 600.0,
+                  enlargeCenterPage: true,
+                  aspectRatio: 16 / 9,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  //autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  viewportFraction: 0.8,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'modeEmploi.dart';
 import 'post.dart';
 import 'textImputWidget.dart';
 import 'postList.dart';
@@ -6,7 +7,6 @@ import 'customListTile.dart';
 import 'customListSwitch.dart';
 import 'substring_highlighted.dart';
 import 'speech_api.dart';
-import 'package:avatar_glow/avatar_glow.dart';
 import 'utils.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -16,7 +16,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<Post> posts = [];
-  String text = 'Cliquez sur le bouton et commencez à parler';
+  String text = '';
   bool isListening = false;
 
   void newPost(String text) {
@@ -50,8 +50,143 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
                 child: Icon(Icons.zoom_out),
               )),
+          Padding(
+              padding: EdgeInsets.only(right: 20.0),
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    posts.clear();
+                  });
+                },
+                child: Icon(Icons.delete),
+              )),
         ],
       ),
+      drawer: //MyDrawer(),
+          Drawer(
+        child: ListView(
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[
+                    Colors.blue[900],
+                    Colors.blue[300],
+                  ],
+                ),
+              ),
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Material(
+                      borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                      elevation: 10,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'images/logo-transparent.png',
+                          width: 80,
+                          height: 80,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Paramètre',
+                        style: TextStyle(color: Colors.white, fontSize: 20.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            CustomListTile(
+                Icons.file_copy_sharp,
+                "Mode d'emploi",
+                () => {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => ModeEmploi()))
+                    }),
+            CustomListTile(
+                Icons.picture_in_picture_sharp, "Fond d'écran", () => {}),
+            CustomListTile(Icons.color_lens_rounded,
+                'Changer couleur du texte vocal', () => {}),
+            CustomListTile(Icons.color_lens_rounded,
+                'Changer couleur du texte écrit', () => {}),
+            CustomListSwitch(Icons.delete, 'Effacer en touchant', Icons.delete),
+            CustomListSwitch(Icons.priority_high_sharp, 'Ponctuation',
+                Icons.priority_high_sharp),
+            CustomListTile(Icons.mail, 'Envoi par Mail', () => {}),
+            CustomListTile(
+                Icons.settings_outlined, 'Paramètres par défaut', () => {})
+          ],
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: PostList(this.posts)),
+          SingleChildScrollView(
+            reverse: true,
+            padding: const EdgeInsets.all(30).copyWith(bottom: 75),
+            child: SubstringHighlight(
+              text: text,
+              terms: Command.all,
+              textStyle: TextStyle(
+                fontSize: 28.0,
+                color: Colors.black,
+                fontWeight: FontWeight.w400,
+              ),
+              textStyleHighlight: TextStyle(
+                fontSize: 32.0,
+                color: Colors.red,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              FloatingActionButton(
+                  child: Icon(Icons.cancel),
+                  mini: true,
+                  backgroundColor: Colors.red,
+                  onPressed: () {
+                    text = 'Appuyez pour parler';
+                  }),
+              FloatingActionButton(
+                child: Icon(isListening ? Icons.mic : Icons.mic_none, size: 36),
+                onPressed: toggleRecording,
+              ),
+              FloatingActionButton(
+                  child: Icon(Icons.check),
+                  mini: true,
+                  backgroundColor: Colors.green,
+                  onPressed: () {
+                    if (text != "") {
+                      newPost(text);
+                    }
+                  }),
+            ],
+          ),
+          TextInputWidget(this.newPost),
+        ],
+      ),
+    );
+  }
+
+  Future toggleRecording() => SpeechApi.toggleRecording(
+      onResult: (text) => this.text = text,
+      onListening: (isListening) {
+        setState(() => this.isListening = isListening);
+      });
+}
+
+/*class MyDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
@@ -106,46 +241,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      body: Column(
-        children: <Widget>[
-          Expanded(child: PostList(this.posts)),
-          SingleChildScrollView(
-            reverse: true,
-            padding: const EdgeInsets.all(30).copyWith(bottom: 150),
-            child: SubstringHighlight(
-              text: text,
-              terms: Command.all,
-              textStyle: TextStyle(
-                fontSize: 32.0,
-                color: Colors.black,
-                fontWeight: FontWeight.w400,
-              ),
-              textStyleHighlight: TextStyle(
-                fontSize: 32.0,
-                color: Colors.red,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-          TextInputWidget(this.newPost),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: AvatarGlow(
-        animate: isListening,
-        endRadius: 75,
-        glowColor: Theme.of(context).primaryColor,
-        child: FloatingActionButton(
-          child: Icon(isListening ? Icons.mic : Icons.mic_none, size: 36),
-          onPressed: toggleRecording,
-        ),
-      ),
     );
   }
-
-  Future toggleRecording() => SpeechApi.toggleRecording(
-      onResult: (text) => this.text = text,
-      onListening: (isListening) {
-        setState(() => this.isListening = isListening);
-      });
 }
+*/
