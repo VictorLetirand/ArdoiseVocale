b815f96a95c2090ce29c9d5e1a3293574c2a5fc7
VictorLetirand <vl.victor@hotmail.fr>
2021-03-24 16:55:50 +0100

    SplashScreen

--- lib/fondEcran.dart
+++ lib/fondEcran.dart
@@ -20,74 +20,88 @@ class _FondEcran extends State<FondEcran> {
                       MaterialPageRoute(builder: (context) => MyHomePage()))
                 }),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Center(
-          child: ListView(
-            children: [
-              CarouselSlider(
-                items: [
-                  //1st Image of Slider
-                  Container(
-                    margin: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      image: DecorationImage(
-                        image: AssetImage("images/flutter-logo.png"),
-                        //fit: BoxFit.fill,
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Center(
+            child: ListView(
+              children: [
+                CarouselSlider(
+                  items: [
+                    //1st Image of Slider
+                    Container(
+                      margin: EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        image: DecorationImage(
+                          image: AssetImage("images/flutter-logo.png"),
+                          //fit: BoxFit.fill,
+                        ),
                       ),
                     ),
-                  ),
 
-                  //2nd Image of Slider
-                  Container(
-                    margin: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      image: DecorationImage(
-                        image: AssetImage("images/logo-final.png"),
-                        //fit: BoxFit.fill,
+                    //2nd Image of Slider
+                    Container(
+                      margin: EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        image: DecorationImage(
+                          image: AssetImage("images/logo-final.png"),
+                          //fit: BoxFit.fill,
+                        ),
                       ),
                     ),
-                  ),
 
-                  //3rd Image of Slider
-                  Container(
-                    margin: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      image: DecorationImage(
-                        image: AssetImage("images/flutter-logo.png"),
-                        //fit: BoxFit.fill,
+                    //3rd Image of Slider
+                    Container(
+                      margin: EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        image: DecorationImage(
+                          image: AssetImage("images/flutter-logo.png"),
+                          //fit: BoxFit.fill,
+                        ),
                       ),
                     ),
-                  ),
 
-                  //4th Image of Slider
-                  Container(
-                    margin: EdgeInsets.all(6.0),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8.0),
-                      image: DecorationImage(
-                        image: AssetImage("images/logo-v2.png"),
-                        //fit: BoxFit.fill,
+                    //4th Image of Slider
+                    Container(
+                      margin: EdgeInsets.all(6.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        image: DecorationImage(
+                          image: AssetImage("images/logo-v2.png"),
+                          //fit: BoxFit.fill,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
 
-                //Slider Container properties
-                options: CarouselOptions(
-                  height: 600.0,
-                  enlargeCenterPage: true,
-                  aspectRatio: 16 / 9,
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enableInfiniteScroll: true,
-                  //autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  viewportFraction: 0.8,
+                  //Slider Container properties
+                  options: CarouselOptions(
+                    height: 600.0,
+                    enlargeCenterPage: true,
+                    aspectRatio: 16 / 9,
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enableInfiniteScroll: true,
+                    //autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    viewportFraction: 0.8,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
+            /*Container(
+                  margin: EdgeInsets.all(25),
+                  child: FlatButton(
+                    child: Text(
+                      'LogIn',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                    color: Colors.blueAccent,
+                    textColor: Colors.white,
+                    onPressed: () {},
+                  ),
+                ),*/
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:ardoise_vocale/myHomePage.dart';
 import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
 
+/*
 void main() {
   runApp(MyApp());
 }
@@ -16,6 +18,44 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(),
+    );
+  }
+}
+*/
+void main() {
+  runApp(new MaterialApp(
+    //debugShowCheckedModeBanner: false,
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: new SplashScreen(
+        title: new Text(
+          'ARDOISE VOCALE',
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+        ),
+        seconds: 6,
+        navigateAfterSeconds: MyHomePage(),
+        image: new Image.asset(
+          'images/logo-splash.gif',
+          alignment: Alignment.center,
+        ),
+        backgroundColor: Colors.grey[350],
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 200.0,
+        onClick: () => Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MyHomePage())),
+        loaderColor: Colors.grey[350],
+      ),
     );
   }
 }
